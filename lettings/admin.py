0ba1eaf60d3c409a7b63a57f4a286f6924d198b3
massouathyassine <massouathfacebook@gmail.com>
2022-06-08 03:28:54 +0200

    Initial commit

---
+++ lettings/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Letting, Address
+
+admin.site.register(Letting)
+admin.site.register(Address)
+
+# resolve interface bug sticky-nav-bar
+admin.autodiscover()
+admin.site.enable_nav_sidebar = False
+from django.contrib import admin
+
+# Register your models here.
